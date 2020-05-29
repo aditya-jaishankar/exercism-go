20074ae8c773577489aabea028f65e3015aa906d
Aditya Jaishankar <aditya.jaishankar@nytimes.com>
2020-05-29 17:37:52 -0400

    add isogram solution

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,1 +1,26 @@
 package isogram
+
+import (
+	"strings"
+)
+
+// IsIsogram implements the solution
+func IsIsogram(word string) bool {
+
+	// Preprocess the words
+	word = strings.ToLower(word)
+	word = strings.Replace(word, " ", "", -1)
+	word = strings.Replace(word, "-", "", -1)
+
+	charCounts := make(map[byte]int)
+	for idx := range word {
+		charCounts[word[idx]]++
+	}
+
+	for key := range charCounts {
+		if charCounts[key] > 1 {
+			return false
+		}
+	}
+	return true
+}
